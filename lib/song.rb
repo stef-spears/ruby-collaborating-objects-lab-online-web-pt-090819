b1b28c94b35b1cad453745631ac2e2a17988f21b
Stef Spears <spears.sj87@gmail.com>
2019-10-13 21:02:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,13 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     song = self.new(song)
+    song.artist_name = artist
     song 
   end 
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song()
+  end
   
 end 
